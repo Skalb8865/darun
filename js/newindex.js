10e8f046955191ff862a1acfafbef17dce652a6b
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2024-12-14 06:58:35 -0500

    Update newindex.js

--- js/newindex.js
+++ js/newindex.js
@@ -1,29 +1,6 @@
-const mobileNav = () => {
-    const headerBtn = document.querySelector('.header__bars');
-    const mobileNav = document.querySelector('.mobile-nav');
-    const mobileLinks = document.querySelectorAll('.mobile-nav__link');
+const openSidebar = document .getElementsByClassName('header-bars')
+const openCart = document .getElementsByClassName('header-cart')
 
-    // State
-    let isMobileNavOpen = false;
 
-    headerBtn.addEventListener('click', () => {
-        isMobileNavOpen = !isMobileNavOpen;
-        if (isMobileNavOpen) {
-            mobileNav.style.display = 'flex';
-            document.body.style.overflowY = 'hidden';
-        } else {
-            mobileNav.style.display = 'none';
-            document.body.style.overflowY = 'auto';
-        }
-    });
-
-    mobileLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            isMobileNavOpen = false;
-            mobileNav.style.display = 'none';
-            document.body.style.overflowY = 'auto';
-        });
-    });
-};
-
-export default mobileNav;
+openSidebar.setAttribute('aria-expanded', 'true')
+openCart.setAttribute('aria-expanded', 'true')
